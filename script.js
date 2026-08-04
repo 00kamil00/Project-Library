8c2d58c87b29e178bd7262e5a2b7da0bb9ec6e9f
00kamix00 <kamilkalemba6@gmail.com>
2026-08-05 00:02:57 +0200

    add displayBooks function to render myLibrary array in DOM

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ function generateId() {
 addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 319)
 addBookToLibrary("Harry Potter", "J.K. Rowling", 328)
 console.log(myLibrary)
+
+
+
+const container = document.querySelector(".container")
+
+function displayBooks() {
+    container.innerHTML = ''
+
+    myLibrary.forEach((item) => {
+        const new_item = document.createElement('div')
+        new_item.classList.add('item')
+
+        new_item.textContent = `${item.title}, ${item.author} with ${item.pages} pages`
+        container.appendChild(new_item)
+    })
+}
+
+displayBooks()
